e1c1747cfe6006de89db7957cee021430f69b73b
Hoang, Marcel Minh Phuc <marcel.hoang@hm.edu>
2026-07-21 20:09:51 +0200

    Add Firebase project config for multiplayer

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -13,13 +13,13 @@
 // ---------------------------------------------------------------------------
 
 export const firebaseConfig = {
-  apiKey: 'YOUR_API_KEY',
-  authDomain: 'YOUR_PROJECT.firebaseapp.com',
-  databaseURL: 'https://YOUR_PROJECT-default-rtdb.firebaseio.com',
-  projectId: 'YOUR_PROJECT',
-  storageBucket: 'YOUR_PROJECT.appspot.com',
-  messagingSenderId: 'YOUR_SENDER_ID',
-  appId: 'YOUR_APP_ID',
+  apiKey: 'AIzaSyD23C35i7MJCOhUpNaJx94hN2ggliQKM80',
+  authDomain: 'tango-duel.firebaseapp.com',
+  databaseURL: 'https://tango-duel-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'tango-duel',
+  storageBucket: 'tango-duel.firebasestorage.app',
+  messagingSenderId: '475486391644',
+  appId: '1:475486391644:web:20528e7b33aea7ca3f4599',
 };
 
 /** True once real keys have been pasted in (used to show a friendly warning). */
